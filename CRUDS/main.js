aac3ca5c4dfc7a56636e1c7a1bd850d968d6667d
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2024-01-26 22:36:29 +0200

    new update
    add new function that create a new product and save it in local storage

--- CRUDS/main.js
+++ CRUDS/main.js
@@ -5,6 +5,9 @@ let taxes = document.querySelector('.tasx');
 let ads = document.querySelector('.ads');
 let discouunt = document.querySelector('.discouunt');
 let total = document.querySelector('.total')
+let count = document.querySelector('.count')
+let category = document.querySelector('.category')
+let create = document.querySelector('.create')
 
 //function to get total price
 function getTotal() {
@@ -13,8 +16,32 @@ function getTotal() {
     if (price.value !== "") {
         total.innerHTML = result
         total.classList.add('result')
+    } else {
+        total.innerHTML = ''
+        total.classList.remove('result')
     }
 
 }
-// console.log('Done')
-// console.log(price.value)
+//function to create a product
+if (localStorage.products != '') {
+    products = JSON.parse(localStorage.products)
+} else {
+
+    let products = []
+}
+create.onclick = function() {
+    let newProduct = {
+        title: title.value,
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discouunt: discouunt.value,
+        count: count.value,
+        category: category.value,
+
+
+    }
+    products.push(newProduct)
+    localStorage.setItem('products', JSON.stringify(products))
+
+}
