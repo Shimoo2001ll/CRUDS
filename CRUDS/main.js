ac9bb775ed3b368e1a20a27dd9a4829727cee7b2
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2024-01-29 18:47:01 +0200

    new update

--- CRUDS/main.js
+++ CRUDS/main.js
@@ -1,4 +1,5 @@
 //declare theinputs
+let inp = document.getElementsByTagName('input')
 let title = document.querySelector('.title');
 let price = document.querySelector('.price');
 let taxes = document.querySelector('.tasx');
@@ -23,25 +24,65 @@ function getTotal() {
 
 }
 //function to create a product
+let products;
 if (localStorage.products != '') {
     products = JSON.parse(localStorage.products)
 } else {
 
-    let products = []
+    products = []
 }
 create.onclick = function() {
-    let newProduct = {
-        title: title.value,
-        price: price.value,
-        taxes: taxes.value,
-        ads: ads.value,
-        discouunt: discouunt.value,
-        count: count.value,
-        category: category.value,
+        let newProduct = {
+            title: title.value,
+            price: price.value,
+            taxes: taxes.value,
+            ads: ads.value,
+            discouunt: discouunt.value,
+            count: count.value,
+            category: category.value,
 
 
+        }
+        products.push(newProduct)
+            //  save to local storage
+        localStorage.setItem('products', JSON.stringify(products))
+        clearData()
+        showData()
     }
-    products.push(newProduct)
-    localStorage.setItem('products', JSON.stringify(products))
+    //cleare date
+function clearData() {
+    title.value = ''
+    price.value = ''
+    discouunt.value = ''
+    taxes.value = ''
+    ads.value = ''
+    count.value = ''
+    category.value = ''
+    total.innerHTML = ''
+    total.classList.remove('result')
 
 }
+
+//  read the dataand display it in html
+function showData() {
+    let table = ''
+    for (let i = 0; i < products.length; i++) {
+        // table = products[i]
+        //     // console.log(table)
+        table += ` 
+                <tr>
+                <td>${i} </td>
+                <td>${products[i].title}</td>
+                <td>${products[i].price}</td>
+                <td>${products[i].taxes}</td>
+                <td>${products[i].ads}</td>
+                <td>${products[i].discouunt}</td>
+                <td>${products[i].count}</td>
+                <td>${products[i].category}</td>
+                <td><button class="btn btn-info"> update</button></td>
+                <td><button class="btn btn-danger"> delete</button></td>
+            </tr>`
+    }
+    document.querySelector('.tableBody').innerHTML = table
+}
+showData()
